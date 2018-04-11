69ed516c5392f7e2b8f7aeeeab6374d92004ad7a
Ryan Meyers <ryan.d.meyers@gmail.com>
2018-04-11 12:15:49 -0400

    add results page

--- components/Results.js
+++ components/Results.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
+import { purple, white, black, gray } from '../utils/colors'
 
 
 class Results extends Component {
@@ -11,13 +12,89 @@ class Results extends Component {
 
     const { deckTitle, deckCards, score } = this.props.navigation.state.params
 
+    const numQuestions = deckCards['questions'].length
+    const percentScore = 100 * score / numQuestions
+    var comment
+
+    if ( percentScore === 100 ) {
+      comment = 'Wow! You got this section perfect!'
+    } else if ( percentScore > 70 ) {
+      comment = 'Not bad! Keep going until you score perfect!'
+    } else {
+      comment = 'Uh oh, looks like this section might need some work!'
+    }
+
     return (
-      <View>
-        <Text>This is the Results</Text>
-        <Text>You got { score } correct!</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>{ deckTitle } Results</Text>
+        <Text style={styles.text}>{ comment }</Text>
+        <Text style={styles.text}>You got { score }/{ numQuestions } correct.</Text>
+        <TouchableOpacity
+          style={[styles.item, {marginTop: 120, backgroundColor: purple}]}
+          onPress={() => this.props.navigation.navigate('Card', {
+            'deckTitle': deckTitle,
+            'cardNum': 0,
+            'score': 0,
+            'deckCards': deckCards
+          })}
+        >
+          <Text
+            style={styles.btnText}
+          >
+            Re-take Quiz
+          </Text>
+        </TouchableOpacity>        <TouchableOpacity
+          style={[styles.item, {backgroundColor: gray}]}
+          onPress={() => this.props.navigation.navigate('DeckList')}
+        >
+          <Text
+            style={styles.btnText}
+          >
+            Back to All Decks
+          </Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 0,
+    justifyContent: 'flex-start'
+  },
+  item: {
+    alignSelf: 'center',
+    backgroundColor: purple,
+    padding: 10,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: white,
+    borderRadius: 10,
+    margin: 20
+  },
+  text: {
+    fontSize: 20,
+    color: purple,
+    margin: 20,
+  },
+  header: {
+    alignSelf: 'center',
+    fontSize: 30,
+    color: purple,
+    marginTop: 40,
+    marginBottom: 40
+  },
+  btnText: {
+    color: white,
+    textAlign: 'center',
+    fontSize: 16
+  },
+
+})
 
 export default Results
